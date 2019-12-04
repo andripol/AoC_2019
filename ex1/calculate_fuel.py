6d8451b252fc1e3eb8a35451ba8c0471ce5a1fab
andri <andriannapolyd@gmail.com>
2019-12-05 00:21:50 +0200

    A bit prettier.

--- ex1/calculate_fuel.py
+++ ex1/calculate_fuel.py
@@ -1,22 +1,15 @@
 def calculate_individual_fuel(mass):
     result = (int(mass/3 - 2))
-    if (result <= 0):
-        return 0 
+    if (result <= 0): return 0 
     else:
         return (result + calculate_individual_fuel(result))
 
 def calculate_fuel():
-    sum = 0
+    masses = []
     with open('input') as f:
-        for line in f:
-            for x in line.split():
-                mass = int(x) 
-                sum = sum + calculate_individual_fuel(mass)
-        print(sum)
+        for x in f.readlines():
+            masses.append(int(x)) 
+    result = sum(map(calculate_individual_fuel, masses))
+    print("Total fuel:", result)
 
-def main():
-    calculate_fuel()
-
-
-if __name__ == "__main__":
-    main()
+calculate_fuel()
