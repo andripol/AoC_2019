1a9cf1b3b293270099e6399913f44e6a91dba319
apolydouri <apolydouri@iit.demokritos.gr>
2019-12-04 15:05:34 +0200

    Refine.

--- ex4/secure_venus.py
+++ ex4/secure_venus.py
@@ -6,33 +6,33 @@ def check_consecutives(i,j,k,l,m,n):
 def find_solution():
     counter = 0
     for i in range(1,7):
-        for j in range(0,10):
+        for j in range(i,10):
             ij = i*100000 + j*10000
-            if (ij < 130000) or (i > j) : 
+            if (ij < 130000): 
                 continue
             if (ij > 670000):
                 break
-            for k in range(0,10):
+            for k in range(j,10):
                 ijk = ij + k*1000
                 if (j > k):
                     continue
                 if (ijk > 678000): 
                     break
-                for l in range(0,10):
+                for l in range(k,10):
                     ijkl = ijk + l*100
-                    if (ijkl < 130200) or (k > l): 
+                    if (ijkl < 130200): 
                         continue
                     if (ijkl > 678200):
                         break
-                    for m in range(0,10):
+                    for m in range(l,10):
                         ijklm = ijkl + m*10
-                        if ijklm < 130250 or (l > m) : 
+                        if ijklm < 130250 : 
                             continue
                         if (ijklm > 678270):
                             break
-                        for n in range(0,10):
+                        for n in range(m,10):
                             ijklmn = ijklm + n
-                            if (ijklmn < 130254 or (m > n)) : 
+                            if (ijklmn < 130254): 
                                 continue
                             if (ijklmn > 678275):
                                 break
