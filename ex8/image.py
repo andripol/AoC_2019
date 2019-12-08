9d65903059e232a414ab8f2b5006a3a03f85f567
andri <andriannapolyd@gmail.com>
2019-12-08 18:14:34 +0200

    Minors

--- ex8/image.py
+++ ex8/image.py
@@ -1,30 +1,22 @@
 global layers
 layers = []
 WIDTH = 25
-HIGHT = 6
+HEIGHT = 6
 
 def input(fname):
     global layers
     with open(fname) as f:
         line = f.readlines()[0].strip('\n')
-        pixels = []
-        for i in range(len(line)):
-            pixels.append(int(line[i]))
-        it = iter(pixels)
-        l = 0
-        while True:
+        for l in range(int(len(line)/(WIDTH*HEIGHT))):
             layers.append([])
-            for i in range(0, HIGHT):
+            offset = l*WIDTH*HEIGHT
+            for i in range(0, HEIGHT):
                 for j in range(0, WIDTH):
-                    pixel = next(it, -1)
-                    if (pixel < 0): 
-                        layers.pop()
-                        return
-                    layers[l].append(pixel)
-            l = l + 1
+                    idx = offset + i*WIDTH + j
+                    layers[l].append(int(line[idx]))
 
 def solve():
-    #min_zeors[count, layer_idx]
+    #min_zeros[count, layer_idx]
     min_zeros = [float('Inf'), -1]
     for i in range(len(layers)):
         if layers[i].count(0) < min_zeros[0]:
@@ -32,17 +24,14 @@ def solve():
     print((layers[min_zeros[1]].count(1)*layers[min_zeros[1]].count(2)))
 
 def solve2():
-    final_layer = []
-    for i in range(0, HIGHT):
-        for j in range(0, WIDTH):
-            final_layer.append(-1)
+    final_layer = [-1]*HEIGHT*WIDTH
 
     for i in range(len(layers)):
         for j in range(len(layers[i])):
             if (final_layer[j] < 0) and (layers[i][j] < 2):
                 final_layer[j] = layers[i][j]
 
-    for i in range(0, HIGHT):
+    for i in range(0, HEIGHT):
         print()
         for j in range(0, WIDTH):
             if (final_layer[i*WIDTH + j] == 0): print(' ', end='') 
