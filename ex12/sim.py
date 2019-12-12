5c0506b1c5c13ce29959a7b92722975098bb8cac
apolydouri <apolydouri@iit.demokritos.gr>
2019-12-12 19:14:19 +0200

    Day 12 Part 1

---
+++ ex12/sim.py
@@ -0,0 +1,38 @@
+import re
+global position, velocity
+
+STEPS = 1000
+
+def input(fname):
+    global position, velocity
+    position = []
+    velocity = []
+    with open(fname) as f:
+        position = [list(map(int,re.findall('-*\d+', line))) for line in f.readlines()]
+    velocity = [[0,0,0], [0,0,0], [0,0,0], [0,0,0]]
+
+def apply_gravity(idx1, idx2, axis):
+    if position[idx1][axis] < position[idx2][axis]:
+        velocity[idx1][axis]+=1
+        velocity[idx2][axis]-=1
+    elif position[idx1][axis] > position[idx2][axis]:
+        velocity[idx1][axis]-=1
+        velocity[idx2][axis]+=1
+
+def apply_velocity(idx):
+    position[idx] = [sum(i) for i in zip(position[idx], velocity[idx])]
+
+def solve():
+    for step in range(0, STEPS):
+        for i in range(0, len(position)):
+            for j in range (i+1, len(position)):
+                for axis in range(0,3):
+                    apply_gravity(i, j, axis)
+            apply_velocity(i)
+
+    potential = [sum(list(map(abs,(position[i])))) for i in range(len(position))]
+    kinetic = [sum(list(map(abs,(velocity[i])))) for i in range(len(velocity))]
+    print("Total: ", sum(potential[i] * kinetic[i] for i in range(len(potential))))
+
+input('input12')
+solve()
