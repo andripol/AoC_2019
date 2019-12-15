c9992f65739e5ba2ef6d530cd12369d3f09997b2
andri <andriannapolyd@gmail.com>
2019-12-15 03:14:33 +0200

    Day 14 Part 2

--- ex14/reactions.py
+++ ex14/reactions.py
@@ -1,7 +1,11 @@
+import math
 # ingredient -> {(quantity, name)}
 reactions = {}
 quantity_quantum = {}
+global remainder 
 remainder = {} 
+
+BILLION = 1000000000000
 
 def input(fname):
     with open(fname) as f:
@@ -18,9 +22,7 @@ def input(fname):
 def solve_rec(ingr):
     quantity = ingr[0]
     name = ingr[1]
-    mul = 0
-    while quantity > mul*quantity_quantum[name] + remainder[name]:
-        mul+=1
+    mul = math.ceil((quantity - remainder[name]) / quantity_quantum[name])
     remainder[name] = (mul*quantity_quantum[name] + remainder[name]) - quantity
     frontier = list(map(lambda x: (x[0]*mul, x[1]),[reactions[name][i] for i in range(len(reactions[name]))]))
     counter = 0
@@ -39,5 +41,30 @@ def solve():
         counter+= solve_rec(i)
     print("Needed ", counter, " ORE for 1 FUEL.")
 
+def solve2():
+    global remainder
+    mul_min = 2463973
+    mul_max = 4000000
+    mul = math.ceil((mul_max + mul_min) / 2)
+    while True:
+        for i in quantity_quantum.keys():
+            remainder[i] = 0
+        print("mul = ", mul)
+        frontier = list(map(lambda x: (x[0]*mul, x[1]),[reactions['FUEL'][i] for i in range(len(reactions['FUEL']))]))
+        counter = 0
+        for i in frontier:
+            counter+= solve_rec(i)
+        print(counter)
+        if (counter < BILLION):
+            mul_min = mul
+            mul = math.ceil((mul_max + mul_min) / 2)
+        elif (counter > BILLION):
+            mul_max = mul
+            mul = math.ceil((mul_max + mul_min) / 2)
+        else:
+            print("Needed ", counter, " ORE for ", mul, " FUEL.")
+            break
+
 input("input14")
 solve()
+solve2()
